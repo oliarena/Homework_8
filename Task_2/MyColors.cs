78be78d11a03e50c0dfee154f3cf623a431d45e3
Olena Iarmusheva <oliar@telenor.dk>
2024-03-29 21:48:01 +0200

    fix comments

--- Task_2/MyColors.cs
+++ Task_2/MyColors.cs
@@ -2,26 +2,6 @@
 {
     internal static class MyColors
     {
-        enum Colors //Як класи, інтерфейси. Enum треба виділяти в окремі файли
-        {
-            Black = 0,
-            DarkBlue = 1,
-            DarkGreen = 2,
-            DarkCyan = 3,
-            DarkRed = 4,
-            DarkMagenta = 5,
-            DarkYellow = 6,
-            Gray = 7,
-            DarkGray = 8,
-            Blue = 9,
-            Green = 10,
-            Cyan = 11,
-            Red = 12,
-            Magenta = 13,
-            Yellow = 14,
-            White = 15
-        }
-
         public static void Print(string text, int color)
         {
             ConsoleColor foregroundColor = Console.ForegroundColor;
